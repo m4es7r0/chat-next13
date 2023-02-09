13d3e71f37699a99de21541125204d110cd3619e
Dmytro Musiichenko <dodogof02@gmail.com>
2023-02-09 15:35:53 +0200

    register page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,23 @@
-import './globals.css'
+import { Inter } from "@next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.className} bg-indigo-300`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="flex justify-center items-center m-0 mx-auto">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
