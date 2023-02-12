6bc16214601b7a3d512fcce1dfcbfa996e9b8097
Dmytro <dmitrymusiichenko@gmail.com>
2023-02-12 15:54:58 +0200

    fix styles

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,7 +1,36 @@
+import Image from "next/image";
 import { FC } from "react";
 
+import styles from "./Chat.module.scss";
+
 const Chat: FC = () => {
-  return <div>Chat</div>;
+  return (
+    <div className={`${styles.chat}`}>
+      <div>
+        <span>Bill M</span>
+        <div>
+          <Image
+            src={"/img/cam.png"}
+            alt={"camera call"}
+            width={1280}
+            height={1280}
+          />
+          <Image
+            src={"/img/add.png"}
+            alt={"add smth"}
+            width={1280}
+            height={1280}
+          />
+          <Image
+            src={"/img/more.png"}
+            alt={"more options"}
+            width={1280}
+            height={1280}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/chats/Chats.tsx
+++ src/components/chats/Chats.tsx
@@ -6,42 +6,20 @@ import styles from "./Chats.module.scss";
 const Chats: FC = () => {
   return (
     <div className={`${styles.chats}`}>
-      <div>
-        <Image
-          src="https://w7.pngwing.com/pngs/7/618/png-transparent-man-illustration-avatar-icon-fashion-men-avatar-face-fashion-girl-heroes-thumbnail.png"
-          width={1270}
-          height={1270}
-          alt="avatar"
-        />
-        <div>
-          <span>Bill M</span>
-          <p>Hello</p>
+      {[...Array(10)].map((el, i) => (
+        <div key={i}>
+          <Image
+            src="https://w7.pngwing.com/pngs/7/618/png-transparent-man-illustration-avatar-icon-fashion-men-avatar-face-fashion-girl-heroes-thumbnail.png"
+            width={1270}
+            height={1270}
+            alt="avatar"
+          />
+          <div>
+            <span>Bill M</span>
+            <p>Hello</p>
+          </div>
         </div>
-      </div>
-      <div>
-        <Image
-          src="https://w7.pngwing.com/pngs/7/618/png-transparent-man-illustration-avatar-icon-fashion-men-avatar-face-fashion-girl-heroes-thumbnail.png"
-          width={1270}
-          height={1270}
-          alt="avatar"
-        />
-        <div>
-          <span>Bill M</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div>
-        <Image
-          src="https://w7.pngwing.com/pngs/7/618/png-transparent-man-illustration-avatar-icon-fashion-men-avatar-face-fashion-girl-heroes-thumbnail.png"
-          width={1270}
-          height={1270}
-          alt="avatar"
-        />
-        <div>
-          <span>Bill M</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
